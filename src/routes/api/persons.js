bd073deb2af5189e5de8f1d88ddc8c8fb640231f
Jhernn127 <javi.hdz1426@gmail.com>
2023-11-21 14:18:34 -0600

    Added Person API Endpoint

---
+++ src/routes/api/persons.js
@@ -0,0 +1,27 @@
+import {Router} from 'express'
+
+const router = router()
+
+router.get('/api', (req, res) => {
+    res.send({ msg: 'Get all Persons' })
+})
+
+
+router.get('/:id', (req, res) => {
+    res.send({ msg: `Getting Person ${req.params.id}` })
+})
+
+router.post('/', (req, res) => {
+    res.send({ msg: 'Creating a new Person' })
+})
+
+router.put('/:id', (req, res) => {
+    res.send({ msg: `Updating Person ${req.params.id}` })
+})
+
+router.delete('/:id', (req, res) => {
+    res.send({ msg: `Deleting Person ${req.params.id}` })
+})
+ 
+
+export default router
